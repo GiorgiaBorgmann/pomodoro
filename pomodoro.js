2658ea1d7370344a4e0d38849032c764f88a6cd8
Giorgia Borgmann <66149711+GiorgiaBorgmann@users.noreply.github.com>
2020-08-31 20:32:50 -0300

    Add files via upload

---
+++ pomodoro.js
@@ -0,0 +1,91 @@
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+
+let wm = document.getElementById('w_minutes');
+let ws = document.getElementById('w_seconds');
+
+let bm = document.getElementById('b_minutes');
+let bs = document.getElementById('b_seconds');
+
+//store a reference to a timer variable
+let startTimer;
+
+start.addEventListener('click', function() {
+    if (startTimer === undefined) {
+        startTimer = setInterval(timer, 1000)
+    } else {
+        alert("Timer is already running");
+    }
+})
+
+reset.addEventListener('click', function() {
+    wm.innerText = 25;
+    ws.innerText = "00";
+
+    bm.innerText = 5;
+    bs.innerText = "00";
+
+    document.getElementById('counter').innerText = 0;
+    stopInterval()
+    startTimer = undefined;
+})
+
+stop.addEventListener('click', function() {
+    stopInterval()
+    startTimer = undefined;
+})
+
+
+//Start Timer Function
+function timer() {
+    //Work Timer Countdown
+    if (ws.innerText != 0) {
+        ws.innerText--;
+    } else if (wm.innerText != 0 && ws.innerText == 0) {
+        ws.innerText = 59;
+        wm.innerText--;
+    }
+
+    //Break Timer Countdown
+    if (wm.innerText == 0 && ws.innerText == 0) {
+        if (bs.innerText != 0) {
+            bs.innerText--;
+        } else if (bm.innerText != 0 && bs.innerText == 0) {
+            bs.innerText = 59;
+            bm.innerText--;
+        }
+    }
+
+    //Increment Counter by one if one full cycle is completed
+    if (wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0) {
+        wm.innerText = 25;
+        ws.innerText = "00";
+
+        bm.innerText = 5;
+        bs.innerText = "00";
+
+        document.getElementById('counter').innerText++;
+    }
+}
+
+//Stop Timer Function
+function stopInterval() {
+    clearInterval(startTimer);
+}
+
+//animation
+
+particlesJS("particles-js", { "particles": { "number": { "value": 388, "density": { "enable": true, "value_area": 710.1992503452364 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 8 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 4.001472541895419, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 6, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "remove" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function() { stats.begin();
+    stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; }
+    requestAnimationFrame(update); };
+requestAnimationFrame(update);;
